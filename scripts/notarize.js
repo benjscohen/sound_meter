a419f22ce810b19dee66519ef6b5df546f7f1095
Benjamin Cohen <benjscohen1@gmail.com>
2025-11-23 20:53:20 -0500

    Moving team ID to secret

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -16,6 +16,6 @@ exports.default = async function notarizing(context) {
         appPath: `${appOutDir}/${appName}.app`,
         appleId: process.env.APPLE_ID,
         appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
-        teamId: '3LVTT3LVDN'
+        teamId: process.env.APPLE_TEAM_ID
     });
 };
