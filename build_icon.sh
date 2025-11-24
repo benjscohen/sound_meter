744daa3aa0a6d7396ee9d1819671e62bb1384e87
Benjamin Cohen <benjscohen1@gmail.com>
2025-11-23 21:06:51 -0500

    Fix app icon shape

--- build_icon.sh
+++ build_icon.sh
@@ -4,16 +4,16 @@
 mkdir sound_meter.iconset
 
 # Resize images
-sips -s format png -z 16 16     icon.png --out sound_meter.iconset/icon_16x16.png
-sips -s format png -z 32 32     icon.png --out sound_meter.iconset/icon_16x16@2x.png
-sips -s format png -z 32 32     icon.png --out sound_meter.iconset/icon_32x32.png
-sips -s format png -z 64 64     icon.png --out sound_meter.iconset/icon_32x32@2x.png
-sips -s format png -z 128 128   icon.png --out sound_meter.iconset/icon_128x128.png
-sips -s format png -z 256 256   icon.png --out sound_meter.iconset/icon_128x128@2x.png
-sips -s format png -z 256 256   icon.png --out sound_meter.iconset/icon_256x256.png
-sips -s format png -z 512 512   icon.png --out sound_meter.iconset/icon_256x256@2x.png
-sips -s format png -z 512 512   icon.png --out sound_meter.iconset/icon_512x512.png
-sips -s format png -z 1024 1024 icon.png --out sound_meter.iconset/icon_512x512@2x.png
+sips -s format png -z 16 16     icon_rounded.png --out sound_meter.iconset/icon_16x16.png
+sips -s format png -z 32 32     icon_rounded.png --out sound_meter.iconset/icon_16x16@2x.png
+sips -s format png -z 32 32     icon_rounded.png --out sound_meter.iconset/icon_32x32.png
+sips -s format png -z 64 64     icon_rounded.png --out sound_meter.iconset/icon_32x32@2x.png
+sips -s format png -z 128 128   icon_rounded.png --out sound_meter.iconset/icon_128x128.png
+sips -s format png -z 256 256   icon_rounded.png --out sound_meter.iconset/icon_128x128@2x.png
+sips -s format png -z 256 256   icon_rounded.png --out sound_meter.iconset/icon_256x256.png
+sips -s format png -z 512 512   icon_rounded.png --out sound_meter.iconset/icon_256x256@2x.png
+sips -s format png -z 512 512   icon_rounded.png --out sound_meter.iconset/icon_512x512.png
+sips -s format png -z 1024 1024 icon_rounded.png --out sound_meter.iconset/icon_512x512@2x.png
 
 # Create .icns file
 iconutil -c icns sound_meter.iconset
